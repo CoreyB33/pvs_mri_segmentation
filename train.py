fbe419474cec12ab9eefdba95c75fe6f03070bce
CoreyB33 <44987324+CoreyB33@users.noreply.github.com>
2020-02-24 16:17:49 -0600

    Update train.py

--- train.py
+++ train.py
@@ -124,17 +124,6 @@ if __name__ == "__main__":
         t1_patches.shape, mask_patches.shape))
 
 
-n_split=3
- 
-for train_index,test_index in KFold(n_split).split(X):
-  x_train,x_test=X[train_index],X[test_index]
-  y_train,y_test=Y[train_index],Y[test_index]
-  
-  model=create_model()
-  model.fit(x_train, y_train,epochs=20)
-  
-  print('Model evaluation ',model.evaluate(x_test,y_test))
-
     ######### TRAINING #########
     history = model.fit(t1_patches,
                         mask_patches,
